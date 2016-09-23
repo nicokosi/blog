55398b449643ac4160d77c313d9aa7f32989f2a8
Nicolas Kosinski <nicokosi@yahoo.com>
2016-09-23 09:24:14 +0200

    Enable Atom feeds discovery
    Via HTML head link.

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 GITHUB_URL = 'http://github.com/nicokosi/'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = 'feeds/all-%s.atom.xml'
 AUTHOR_FEED_ATOM = 'feeds/%s.atom.xml'
